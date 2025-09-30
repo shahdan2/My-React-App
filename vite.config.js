360fe37fcf241d9ab2301fc1f6416c548266faa8
shahdan2 <sariya.munsif@gmail.com>
2025-09-30 21:24:04 +0530

    Done

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "My-React-App"
+  base: "/My-React-App/"
   plugins: [react()],
   server: {
     port: 3000,           // Optional: change dev server port
