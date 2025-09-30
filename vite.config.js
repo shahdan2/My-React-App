f9791e255222edad59c4f8ab7c737833babbdc77
shahdan2 <sariya.munsif@gmail.com>
2025-09-30 21:17:56 +0530

    React App Done

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "My-React-App"
+  plugins: [react()],
+  server: {
+    port: 3000,           // Optional: change dev server port
+    open: true,           // Optional: auto-open in browser
+  },
+  resolve: {
+    alias: {
+      '@': '/src',        // Optional: use @ as alias for /src folder
+    },
+  },
+});
